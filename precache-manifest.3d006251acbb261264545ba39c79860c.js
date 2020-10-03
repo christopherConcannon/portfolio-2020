3d33721761747eddbfec2b9cbc7648aad943ba4e
xtophe xanon <xocon10@gmail.com>
2020-10-03 17:48:34 -0500

    Updates

---
+++ precache-manifest.3d006251acbb261264545ba39c79860c.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9851fcb6ec5c19c5c05466aaa87ea70",
+    "url": "/deep-thoughts/index.html"
+  },
+  {
+    "revision": "bc1896c7b00eaaacc242",
+    "url": "/deep-thoughts/static/css/main.ce5637ec.chunk.css"
+  },
+  {
+    "revision": "1c079148a741a8e8a171",
+    "url": "/deep-thoughts/static/js/2.37de2f78.chunk.js"
+  },
+  {
+    "revision": "bfd04eb851fbc8ea43c375cc9d698376",
+    "url": "/deep-thoughts/static/js/2.37de2f78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc1896c7b00eaaacc242",
+    "url": "/deep-thoughts/static/js/main.ec7c8301.chunk.js"
+  },
+  {
+    "revision": "2038c7c6fb7841e5fd30",
+    "url": "/deep-thoughts/static/js/runtime-main.5efec8fe.js"
+  },
+  {
+    "revision": "48dfdba3eb0559af9ddb34cc51d06aa5",
+    "url": "/deep-thoughts/static/media/EdgeLedger.48dfdba3.jpg"
+  },
+  {
+    "revision": "f8fc027b4d8e0ad97c02feacdfc5d588",
+    "url": "/deep-thoughts/static/media/calavera.f8fc027b.svg"
+  },
+  {
+    "revision": "28b79212adcdaf363523892deb20154a",
+    "url": "/deep-thoughts/static/media/call-it-a-night-screenshot.28b79212.jpg"
+  },
+  {
+    "revision": "5d51cda837b7dc3343942d7eeccd5645",
+    "url": "/deep-thoughts/static/media/coding-assessment.5d51cda8.jpg"
+  },
+  {
+    "revision": "1a743d795acae2f7be1bbbc83be656d7",
+    "url": "/deep-thoughts/static/media/color-palette-customizer.1a743d79.jpg"
+  },
+  {
+    "revision": "6de49159181ae874b4d1a5755f5e8f64",
+    "url": "/deep-thoughts/static/media/dev-connector.6de49159.jpg"
+  },
+  {
+    "revision": "af154a6c4229509a7934c8d1d73e4218",
+    "url": "/deep-thoughts/static/media/employee-database.af154a6c.jpg"
+  },
+  {
+    "revision": "4fca8a15398504b99ed81a393df5e9be",
+    "url": "/deep-thoughts/static/media/glozzom.4fca8a15.jpg"
+  },
+  {
+    "revision": "ca9702c8dc1b8a41aa0385012f4b8e58",
+    "url": "/deep-thoughts/static/media/instartist.ca9702c8.jpg"
+  },
+  {
+    "revision": "414e9adfb1f91e1d40f88eaf9e4a4385",
+    "url": "/deep-thoughts/static/media/loopLab.414e9adf.jpg"
+  },
+  {
+    "revision": "5c320b8766a190cd984cb4657df71234",
+    "url": "/deep-thoughts/static/media/newsgrid.5c320b87.jpg"
+  },
+  {
+    "revision": "af7e8f973826103f87ebcce67a393f91",
+    "url": "/deep-thoughts/static/media/password-generator.af7e8f97.jpg"
+  },
+  {
+    "revision": "1010dd796df9bde132d79b211261a5ce",
+    "url": "/deep-thoughts/static/media/run-buddy.1010dd79.jpg"
+  },
+  {
+    "revision": "1524ddca1f19ea2ba427a170d2aa1b25",
+    "url": "/deep-thoughts/static/media/screenshot.1524ddca.jpg"
+  },
+  {
+    "revision": "d5a378c2c784b8edf5ddc272a93e424d",
+    "url": "/deep-thoughts/static/media/taskinator.d5a378c2.jpg"
+  },
+  {
+    "revision": "319ae074da068027ecde7723267059b9",
+    "url": "/deep-thoughts/static/media/team-profile-generator.319ae074.jpg"
+  },
+  {
+    "revision": "f1156307928bb6fa99832e0993cb6f2c",
+    "url": "/deep-thoughts/static/media/weather-dashboard.f1156307.jpg"
+  },
+  {
+    "revision": "728048ddfd3242a87af8c0c7d1d14ec5",
+    "url": "/deep-thoughts/static/media/web-sized-portrait.728048dd.jpg"
+  },
+  {
+    "revision": "74abc6003cd4cbd11eb83e6533508a25",
+    "url": "/deep-thoughts/static/media/work-day-scheduler.74abc600.jpg"
+  },
+  {
+    "revision": "c23067209ec137111d53404c4f88d621",
+    "url": "/deep-thoughts/static/media/yelp-camp.c2306720.jpg"
+  }
+]);
---
+++ script.js
@@ -0,0 +1,12 @@
+// NAVIGATION COLOR FADE ON SCROLL...MAYBE ADD TO USEEFFECT HOOK IN NAV COMPONENT TO SIMULATE COMPONENT DID MOUNT
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 44) {
+    document.querySelector('.Navigation-navbar').classList.add('faded-header')
+  } else {
+    document.querySelector('.Navigation-navbar').classList.remove('faded-header')
+  }
+})
+
+
+
+
