d37dc0423f81fcf0fd262d72e9fb72dcd83aee1e
xtophe xanon <xocon10@gmail.com>
2020-11-24 16:50:19 -0600

    prepare for deploy

--- server/server.js
+++ server/server.js
@@ -78,4 +78,15 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use('/', router)
+
+// serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')))
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'))
+})
+
+
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
